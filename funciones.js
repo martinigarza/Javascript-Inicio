56c84f8f3c08ca5a621cbc53f51159b1fec5e70c
martinigarza <martinigarza10@gmail.com>
2023-01-24 12:47:08 -0300

    Seguimos practicando Javascript

---
+++ funciones.js
@@ -0,0 +1,22 @@
+//declarativas
+
+function MiFuncion(){
+    return 3;
+}
+
+MiFuncion();
+
+//Expresion
+
+var MiFuncion = function(a,b){
+    return a + b;
+}
+
+MiFuncion();
+
+function sumar(a,b){
+    var resultado=a + b;
+    return resultado;
+}
+
+
---
+++ variables.js
@@ -0,0 +1,19 @@
+var nombre = "Oscar"; 
+//-----------------------
+//declarar
+var edad;
+//inicializar
+edad = 30;
+
+var elementos = ["Computadora", "Celular"];
+
+var persona ={
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
+
+elementos[1];
+
+
